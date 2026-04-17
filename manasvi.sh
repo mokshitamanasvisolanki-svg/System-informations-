24a26f4ad959745745c6b189d024326f17caeb31
mokshitamanasvisolanki-svg <mokshitamanasvisolanki@gmail.com>
2026-04-17 20:33:16 +0530

    Add files via upload

---
+++ manasvi.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "------ SYSTEM INFORMATION ------"
+
+echo "Date & Time:"
+date
+
+echo "--------------------------------"
+
+echo "Logged in User:"
+whoami
+
+echo "--------------------------------"
+
+echo "Current Directory:"
+pwd
+
+echo "--------------------------------"
+
+echo "System Uptime:"
+uptime
+
+echo "--------------------------------"
+
+echo "Disk Usage:"
+df -h
+
+echo "--------------------------------"
+
+echo "Memory Usage:"
+free -h
+
+echo "--------------------------------"
+
+echo "Files in Current Directory:"
+ls
+
+echo "--------------------------------"
